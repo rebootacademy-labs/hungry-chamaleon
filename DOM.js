2db5a1aae359c372c162c27df9b1930f6bcc734c
Carla Ros <krla85@carla.localdomain>
2020-02-24 16:26:00 +0000

    hemos conseguido las posiciones aleatorias

--- DOM.js
+++ DOM.js
@@ -2,14 +2,10 @@ var wrapperGrasshopper = document.getElementById("wrapper-grasshopper")
 
 
 var timerId = setInterval(function(){
-  var x = function getpositionArbitrary(x) {
-    return Math.random() * (0 - 1000);
-  };
-  var y = function getpositionArbitrary(y) {
-    return Math.random() * (0 - 630);
-  };
-  var newGrasshopper2 = new Grasshopper("red", x, y)
-  var divGrasshopper2 = newGrasshopper2.show()
-  wrapperGrasshopper.appendChild(divGrasshopper2)
+  var x = Math.floor(Math.random()*(1000-120));
+  var y = Math.floor(Math.random()*(430-80));
+  var newGrasshopper = new Grasshopper("red", x, y)
+  var divGrasshopper = newGrasshopper.show()
+  wrapperGrasshopper.appendChild(divGrasshopper)
 }, 5000)
 
