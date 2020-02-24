1a32cad10b4ecd4b25e6a1ca93fad54ca1905d46
Cristina Déniz <cdenizperdomo@gmail.com>
2020-02-24 20:22:29 +0000

    puestos los saltamontes con colores aleatorios y tambien el camaleon con color aleatorio

--- DOM.js
+++ DOM.js
@@ -1,11 +1,21 @@
 var wrapperGrasshopper = document.getElementById("wrapper-grasshopper")
 
-
-var timerId = setInterval(function(){
+var timerIdGrasshopper = setInterval(function(){
   var x = Math.floor(Math.random()*(1000-120));
   var y = Math.floor(Math.random()*(430-80));
-  var newGrasshopper = new Grasshopper("red", x, y)
+  var colors = ["red", "green", "blue", "yellow"]
+  var randomNumber = Math.floor(Math.random()*4)
+  var newGrasshopper = new Grasshopper(colors[randomNumber], x, y)
   var divGrasshopper = newGrasshopper.show()
   wrapperGrasshopper.appendChild(divGrasshopper)
 }, 5000)
 
+var wrapperChamaleon = document.getElementById("wrapper-chamaleon")
+
+var timerIdChamaleon = setInterval(function(){
+  var colores = ["rojo", "verde", "azul", "amarillo"]
+  var randomNumbers = Math.floor(Math.random()*4);
+  var newChamaleon = new Chamaleon(colores[randomNumbers]);
+  var divChamaleon = newChamaleon.show();
+  wrapperChamaleon.appendChild(divChamaleon);
+}, 5000)
--- index.js
+++ index.js
@@ -5,14 +5,26 @@ function Grasshopper(color, posX, posY) {
   this.posY = posY;
 
   this.show = function () {
+    this.html.classList.add(this.color);
     this.html.classList.add("grasshopper");
     this.html.style.top = this.posY+"px";
     this.html.style.left = this.posX+"px";
     console.log(this.html)
     var that = this;
     this.html.onclick = function () {
-      that.html.classList.add("hide");
+      that.html.remove()
     }
     return this.html;
+  }
+}
+
+function Chamaleon(color) {
+  this.html = document.createElement("div")
+  this.color = color;
+
+  this.show = function () {
+    this.html.classList.add(this.color);
+    this.html.classList.add("chamaleon");
+    return  this.html;
   }
 }
